f13b00d873920af1e8286553f25668b064f21f67
Julien He <32774836+hulien22@users.noreply.github.com>
2018-09-22 22:22:34 -0400

    Adding simple test to just test the genetic algorithm/neural network

--- SimpleNeuralNetwork/Settings.h
+++ SimpleNeuralNetwork/Settings.h
@@ -7,17 +7,17 @@
 #define CROSSOVER_RATE 0.7
 #define ELITISM_RATE 0.5
 
-#define NUMBER_OF_INPUTS 25
+#define NUMBER_OF_INPUTS 4
 #define NUMBER_OF_OUTPUTS 3
 
-#define HIDDEN_LAYERS 10
+#define HIDDEN_LAYERS 1
 #define HIDDEN_LAYER_SIZE 10
 
 #define BOARD_X 20
 #define BOARD_Y 20
 #define INITIAL_SNAKE_SIZE 5
 
-#define POPULATION_SIZE 10
+#define POPULATION_SIZE 1000
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "SimpleNeuralNetwork/Settings.h"
 #include "SimpleNeuralNetwork/GeneticAlgorithm.h"
-#include "SimpleNeuralNetwork/NeuralNetwork.h"
 #include "Snake/game.h"
 
 #include <fstream>
@@ -36,70 +35,93 @@ int main(int argc, char** argv) {
 
         // run each genome through the game to get their fitness scores
         for (int j=0; j<POPULATION_SIZE; ++j) {
-            // std::cout << "*****************************************" << std::endl;
-            // std::cout << "Training Species " << j << " of Generation " << i << "." << std::endl;
-            double fitnessAverage = 0;
-            for (int k=0; k<5; ++k) {
-                int steps = 0, totalSteps = 0, facing = 0, size = INITIAL_SNAKE_SIZE;
-                bool validMove, loop = true;
-                std::vector<double> outputs(NUMBER_OF_OUTPUTS);
 
-                game.reset(INITIAL_SNAKE_SIZE);
-
-                nnet.SetWeights(genomes[j].getWeights());
-
-                while(loop) {
-                    outputs = nnet.Calculate(game.getInputVector());
-                    facing = game.getFacing();
-                    // 3 outputs
-                    if (outputs[0] > outputs[1] && outputs[0] > outputs[2]) {
-                        ++facing; //turn left
-                    } else if (outputs[2] > outputs[0] && outputs[2] > outputs[1]) {
-                        --facing; //turn right
-                    } //else go straight
-                    facing = (facing % 4 + 4) % 4;
-                    validMove = game.move(facing);
-                    ++steps;
-
-                    if (!validMove) {
-                        // Fitness is based on survival time and size, size is weighted much more
-                        // genomes[j].setFitness(totalSteps + steps + 100*(game.getSize() - INITIAL_SNAKE_SIZE));
-                        
-                        fitnessAverage += (game.getSize() - INITIAL_SNAKE_SIZE) * (game.getSize() - INITIAL_SNAKE_SIZE) ;
-                        loop = false;
-                    } else {
-                        if (game.getSize() > size) {
-                            totalSteps += steps;
-                            steps = 0;
-                            ++size;
-                        }
-                        if (steps >= 100) loop = false; //not going anywhere, kill genome
-                    }
-                }
-            
-            }
-            genomes[j].setFitness(fitnessAverage/5.0);
-            // for (size_t q=0; q<genomes[j].getWeights().size(); ++q) {
-            //     std::cout << genomes[j].getWeights()[q] << ", ";
+            // std::vector<double> in = {1,0,1,-1,-0.5,1,0,1,-1,-0.5,1,0,1,-1,-0.5,1,0,1,-1,-0.5,1,0,1,-1,-0.5};
+            std::vector<double> in = {1,0,1,-1};
+            std::vector<double> outputs(NUMBER_OF_OUTPUTS);
+            std::vector<double> w = genomes[j].getWeights();
+            // for (size_t q=0; q<w.size(); ++q){
+            //     std::cout << w[q] << ", ";
             // }
             // std::cout << std::endl;
+            nnet.SetWeights(w);
+            outputs = nnet.Calculate(in);
+            genomes[j].setFitness(outputs[0] + outputs[1] - outputs[2]);
+
+            // std::cout << "*****************************************" << std::endl;
+            // std::cout << "Training Species " << j << " of Generation " << i << "." << std::endl;
+            // double fitnessAverage = 0;
+            // for (int k=0; k<5; ++k) {
+            //     int steps = 0, totalSteps = 0, facing = 0, size = INITIAL_SNAKE_SIZE;
+            //     bool validMove, loop = true;
+            //     std::vector<double> outputs(NUMBER_OF_OUTPUTS);
+
+            //     game.reset(INITIAL_SNAKE_SIZE);
+
+            //     nnet.SetWeights(genomes[j].getWeights());
+
+            //     while(loop) {
+            //         outputs = nnet.Calculate(game.getInputVector());
+            //         facing = game.getFacing();
+            //         // 3 outputs
+            //         if (outputs[0] > outputs[1] && outputs[0] > outputs[2]) {
+            //             ++facing; //turn left
+            //         } else if (outputs[2] > outputs[0] && outputs[2] > outputs[1]) {
+            //             --facing; //turn right
+            //         } //else go straight
+            //         facing = (facing % 4 + 4) % 4;
+            //         validMove = game.move(facing);
+            //         ++steps;
+
+            //         if (!validMove) {
+            //             // Fitness is based on survival time and size, size is weighted much more
+            //             // genomes[j].setFitness(totalSteps + steps + 100*(game.getSize() - INITIAL_SNAKE_SIZE));
+                        
+            //             fitnessAverage += (game.getSize() - INITIAL_SNAKE_SIZE) * (game.getSize() - INITIAL_SNAKE_SIZE) ;
+            //             loop = false;
+            //         } else {
+            //             if (game.getSize() > size) {
+            //                 totalSteps += steps;
+            //                 steps = 0;
+            //                 ++size;
+            //             }
+            //             if (steps >= 100) loop = false; //not going anywhere, kill genome
+            //         }
+            //     }
+            
+            // }
+            // genomes[j].setFitness(fitnessAverage/5.0);
             // std::cout << "Fitness Score: " << genomes[j].getFitness() << " | Genome took " << totalSteps + steps << " steps, and grew to a size of " << size << std::endl;
             // std::cout << "*****************************************" << std::endl;
         }
 
         // now use ga to calculate next generation
+        std::vector<Genome> oldg;
+        for (size_t q=0; q<genomes.size(); ++q){
+            oldg.emplace_back(genomes[q].getWeights(), genomes[q].getFitness());
+        }
+
         ga.Step(genomes);
+
+        bool check = true;
+        for (size_t q=0; q<genomes.size(); ++q){
+            if(check && oldg[q].getFitness() == genomes[q].getFitness()) check = true;
+            else check = false;
+        }
+        
+        std::cout << (check ? "TRUE" : "FALSE") << std::endl;
+
         Genome bestGenome = ga.getBestGenomeOfGeneration();
 
         //save best genome to file
         std::vector<double> bestweights = bestGenome.getWeights();
-        fs.open("out.txt", std::fstream::ate);
-        fs << bestGenome.getFitness() << "| ";
-        for (size_t j=0; j<bestweights.size(); ++j) {
-            fs << bestweights[j] << " ";
-        }
-        fs << std::endl;
-        fs.close();
+        // fs.open("out.txt", std::fstream::ate);
+        // fs << bestGenome.getFitness() << "| ";
+        // for (size_t j=0; j<bestweights.size(); ++j) {
+        //     fs << bestweights[j] << " ";
+        // }
+        // fs << std::endl;
+        // fs.close();
 
         std::cout << "#########################################" << std::endl;
         std::cout << "Generation " << i << " completed." << std::endl;
